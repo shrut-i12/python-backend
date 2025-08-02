6104dc0c1ba3648e2d70bba11da68355e27d08dd
shrut-i12 <shrutipalankar1201@gmail.com>
2025-08-02 10:27:25 +0530

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/<int:n>")
+def even(n):
+    primes=""
+    for i in range(1,n+1):
+        for n in range(2,i):
+          if(i%n==0):
+             break
+        else:
+           primes = primes + str(i)+","
+    return primes
+if __name__ == "__main__":
+    app.run()
+        
+        
